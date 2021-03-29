10c7152b7ac4fd6242d9edbd4300efb9df9f47aa
Roberto Mazzotta <roberto.mazzotta@basenet.ch>
2021-03-29 13:39:56 +0200

    extend catalog

--- controller.go
+++ controller.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func clearTerminal() {
@@ -21,19 +23,82 @@ func readJsonFile() {
 	_ = json.Unmarshal([]byte(file), &data)
 
 	for i := 0; i < len(data.Questions); i++ {
-		fmt.Println("Frage: ", i+1)
-		fmt.Println("ID: ", data.Questions[i].Id)
-		fmt.Println("Text:", data.Questions[i].Question)
-		fmt.Println("Difficulty:", data.Questions[i].Difficulty)
-		fmt.Println("\n")
+		// fmt.Println("Frage: ", i+1)
+		// fmt.Println("ID: ", data.Questions[i].Id)
+		// fmt.Println("Text:", data.Questions[i].Question)
+		// fmt.Println("Difficulty:", data.Questions[i].Difficulty)
 
 		for y := 0; y < len(data.Questions[i].Answers); y++ {
-			answer := data.Questions[i].Answers[y]
+			//answer := data.Questions[i].Answers[y]
 
-			fmt.Println("Antwort: ", y+1)
-			fmt.Println("Text: ", answer.Text)
-			fmt.Println("Korrekt:", answer.Correct)
-			fmt.Println("\n")
+			// fmt.Println("Antwort: ", y+1)
+			// fmt.Println("Text: ", answer.Text)
+			// fmt.Println("Korrekt:", answer.Correct)
 		}
 	}
+}
+
+func startQuiz() {
+	printStartQuiz()
+
+	for {
+		executeCommand()
+	}
+
+	// command := askForCommand()
+	// if command != "s" {
+	// 	os.Exit(3)
+	// }
+	// fmt.Println("Frage Nummer 01")
+}
+
+func executeCommand() {
+	command := askForCommand()
+	parseCommand(command)
+}
+
+func parseCommand(input string) {
+	switch {
+	case input == "s":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "":
+		break
+	case input == "q":
+		clearTerminal()
+		printExitQuiz()
+		os.Exit(0)
+	}
+}
+
+func askForCommand() string {
+	reader := bufio.NewReader(os.Stdin)
+	response, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	response = strings.TrimSpace(response)
+	return response
 }
--- view.go
+++ view.go
@@ -20,13 +20,25 @@ func printMenue() {
 ******* Das Quiz besteht aus 10 Fragen ***********
 ******* Pro Quiz können 2 Joker gewählt werden ***
 **************************************************
-# Übersicht Commands
-# a-d: Antwort auswählen
-# 5: 50/50 Joker auswählen
-# r: Retry Joker auswählen
-# q. Quiz beenden`)
+**** Übersicht Commands ****
+s  : Quiz starten
+a-d: Antwort auswählen
+n  : Nächste Frage
+5  : 50/50 Joker auswählen
+r  : Retry Joker auswählen
+q  : Quiz beenden`)
 	fmt.Println(string(colorReset))
 
 	fmt.Println(string(colorRed), "test")
+	fmt.Println(string(colorReset))
+}
+
+func printStartQuiz() {
+	fmt.Println(string(colorYellow), "Starte Quiz mit 's'")
+	fmt.Println(string(colorReset))
+}
+
+func printExitQuiz() {
+	fmt.Println(string(colorYellow), "Quiz wurde beendet!")
 	fmt.Println(string(colorReset))
 }
